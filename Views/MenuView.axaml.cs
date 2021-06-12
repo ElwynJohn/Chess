c99ee9e11429727e06369c1ed0aba470c2265908
Elwyn John <69361511+ElwynJohn@users.noreply.github.com>
2021-06-12 18:40:45 +0100

    Add method that navigates to Play view

--- Views/MenuView.axaml.cs
+++ Views/MenuView.axaml.cs
@@ -18,7 +18,14 @@ namespace Chess.Views
             AvaloniaXamlLoader.Load(this);
         }
 
-        public void on_pointer_released_history(object sender, RoutedEventArgs e)
+        public void on_click_play(object sender, RoutedEventArgs e)
+        {
+            MainWindowViewModel model = (MainWindowViewModel)((Control)sender).FindAncestorOfType<Window>().DataContext;
+            string fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+            model.List = new PlayViewModel(fen);
+        }
+
+        public void on_click_history(object sender, RoutedEventArgs e)
         {
             MainWindowViewModel model = (MainWindowViewModel)((Control)sender).FindAncestorOfType<Window>().DataContext;
             string[] gameHistorys = new string[]
