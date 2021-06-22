e538dc3d2ec79938778bc0e64c1a4d72f5882523
rleathart <56587304+rleathart@users.noreply.github.com>
2021-06-22 18:54:34 +0100

    Convert ChessMove back to 64 based indexing

--- Models/ChessMove.cs
+++ Models/ChessMove.cs
@@ -4,20 +4,20 @@ namespace Chess.Models
 {
     public class ChessMove
     {
-        /// <summary>0x88 based origin/target positions</summary>
+        /// <summary>64 based origin/target positions</summary>
         public byte[] data = new byte[2];
 
         /// <summary>64 based origin position</summary>
         public int From
         {
-            get => Pos64(data[0]);
-            set => data[0] = (byte)Pos88(value);
+            get => data[0];
+            set => data[0] = (byte)value;
         }
         /// <summary>64 based target position</summary>
         public int To
         {
-            get => Pos64(data[1]);
-            set => data[1] = (byte)Pos88(value);
+            get => data[1];
+            set => data[1] = (byte)value;
         }
 
         public ChessMove()
@@ -26,7 +26,6 @@ namespace Chess.Models
 
         public ChessMove(int from, int to, bool pos0x88 = false)
         {
-            System.Console.WriteLine($"ChessMove ctor: {from} {to}");
             if (pos0x88)
             {
                 from = ChessBoard.Pos64(from);
