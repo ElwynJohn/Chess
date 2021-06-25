ab0384c945f3a4312ed2ea40100a369a2181c44e
rleathart <56587304+rleathart@users.noreply.github.com>
2021-06-25 22:49:29 +0100

    Read server_move async
    This means we no longer have to wait for the server to compute its move
    before drawing the client's move in the GUI. Instead the client move can
    be drawn instantly and then we set IsInteractable to false until the
    client receives the server's move

--- ViewModels/AIBoardViewModel.cs
+++ ViewModels/AIBoardViewModel.cs
@@ -1,6 +1,7 @@
 using Chess.Models;
 using System.IO.Pipes;
 using System;
+using System.Threading.Tasks;
 
 namespace Chess.ViewModels
 {
@@ -24,15 +25,25 @@ namespace Chess.ViewModels
             {
                 byte[] buf = new byte[2];
                 client.Write(move.data, 0, 2);
-                client.Read(buf, 0, 2);
-                ChessMove server_move = new ChessMove(buf);
-                Console.WriteLine("Got move: {0}", server_move);
-                board[server_move.To] = board[server_move.From];
-                board[server_move.From] = ChessPiece.None;
-                isWhitesMove = !isWhitesMove;
-                Moves.Add(server_move);
-                AddMoveToTurns(server_move);
-                currentMove++;
+
+                Task t = client.ReadAsync(buf, 0, 2);
+                // We shouldn't be able to do anything while the server is thinking
+                // @@FIXME This seems to have weird behaviour if you try to move
+                // a piece while the server is thinking
+                IsInteractable = false;
+
+                t.ContinueWith((readTask) =>
+                {
+                    ChessMove server_move = new ChessMove(buf);
+                    Console.WriteLine("Got move: {0}", server_move);
+                    board[server_move.To] = board[server_move.From];
+                    board[server_move.From] = ChessPiece.None;
+                    isWhitesMove = !isWhitesMove;
+                    Moves.Add(server_move);
+                    AddMoveToTurns(server_move);
+                    currentMove++;
+                    IsInteractable = true;
+                });
             }
             Console.WriteLine(board);
         }
