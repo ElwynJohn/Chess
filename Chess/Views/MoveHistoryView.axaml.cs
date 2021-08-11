aac7e959498dcc367f785bd7d5b0234f2fdf939c
Elwyn John <69361511+ElwynJohn@users.noreply.github.com>
2021-08-11 10:58:55 +0100

    Bugfix: scroll bar doesn't track latest move.
    Closes #46

--- Chess/Views/MoveHistoryView.axaml.cs
+++ Chess/Views/MoveHistoryView.axaml.cs
@@ -1,7 +1,9 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Interactivity;
 using Chess.ViewModels;
+using Chess.Models;
 
 namespace Chess.Views
 {
@@ -10,6 +12,24 @@ namespace Chess.Views
         public MoveHistoryView()
         {
             InitializeComponent();
+        }
+
+        public void SvOnInitialized(object sender, EventArgs e)
+        {
+            var vm = DataContext as MoveHistoryViewModel;
+            if (vm == null)
+            {
+                Logger.EWrite("Cast failed.");
+                return;
+            }
+            var sv = sender as ScrollViewer;
+            if (sv == null)
+            {
+                Logger.EWrite("Cast failed.");
+                return;
+            }
+            vm.sv = sv;
+            sv.LayoutUpdated += vm.SvLayoutUpdated;
         }
 
         public void previous_move(object sender, RoutedEventArgs e)
